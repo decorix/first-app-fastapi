07ea8019b1cd19c92fdc348d95d3c4a9db840352
decorix <polevg2015@yandex.ru>
2024-11-03 20:11:13 +0300

    feature (database.py) configure database

--- app/main.py
+++ app/main.py
@@ -1,33 +1,30 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
 
 app = FastAPI()
 
-class SHotel(BaseModel):
-    address: str
-    name: str
-    stars: int
-    has_spa: bool
-    
-@app.get("/hotels", response_model=list[SHotel])
+class HotelsSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date, 
+        date_to: date,
+        has_space: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_space = has_space
+        self.stars = stars
+
+@app.get("/hotels")
 def get_hotels(
-    location: str,
-    date_from: date, 
-    date_to: date,
-    has_space: Optional[bool] = None,
-    stars: Optional[int] = Query(None, ge=1, le=5),
+    search_args: HotelsSearchArgs = Depends()
 ):
-    hotels = [
-        {
-            "address": "ул. Гагарина",
-            "name": "Super Hotel",
-            "stars": 5,
-            "has_spa": True
-        }
-    ]
-    return hotels
+    return search_args
 
 class SBooking(BaseModel):
     room_id: int
